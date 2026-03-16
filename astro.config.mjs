a77f5df7aa126f5e47e8904fb7e358638bbd576f
GabrielReisSantos <gabriiel.reiis@hotmail.com>
2026-03-16 15:31:02 +0100

    Switch site URL to gabrielreisantos.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://gabrielsmind.io',
+  site: 'https://gabrielreissantos.com',
 });
